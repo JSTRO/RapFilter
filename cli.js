d9c7ae7c7f398acd26e999627b755ae24685b268
Joseph Ostrow <jstro@umich.edu>
2015-10-02 21:51:31 -0400

    checks if given word is naughty

--- cli.js
+++ cli.js
@@ -4,9 +4,10 @@ console.log(songQuery);
 var genius = require('rapgenius-js');
 
 function songCallback (err, songs, lyricsAndExplanations) {
-	if(err){
+	if(err) {
     console.log("Error: " + err);
-  } else{
+    } 
+    else {
   		if (songs.length > 0) {
   			genius.searchLyricsAndExplanations(songs[0].link, "rap", lyricsSearchCb)
   		}
@@ -17,13 +18,45 @@ function songCallback (err, songs, lyricsAndExplanations) {
 };
 
 function lyricsSearchCb(err, lyricsAndExplanations){
-    if(err){
+    if(err) {
       console.log("Error: " + err);
-    }else{
-         var lyrics = lyricsAndExplanations.lyrics;
-      
-      console.log("**** LYRICS *****\n%s", lyrics.getFullLyrics(true));
     }
+    else {
+         var lyrics = lyricsAndExplanations.lyrics;
+      	 lyrics.getFullLyrics(true);
+    }
+    var str = lyrics.getFullLyrics(true);
+    var strNoPunc = str.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var whiteSpacePattern = /\s+/g;
+    var strFinal = strNoPunc.split(whiteSpacePattern);
+	console.log(strFinal);
+
+	var request = require('request');
+
+	var baseUrl = 'http://www.wdyl.com/profanity?q=';
+	var firstWord = strFinal[13];
+	
+	request({ 
+	url: baseUrl + firstWord
+		}, 
+		
+	function callback (err, response, body) {
+		body = JSON.parse(body);
+		if (err) {
+			console.log('oops there was an error');
+			throw err;
+		}
+		else {
+			if (body.response === "true") {
+				console.log(firstWord + " " + "is a naughty word!");
+			} 
+			else {
+				console.log(firstWord + " " + "is fine by me!")
+			}
+			console.log(body);
+		}
+});
+
 };
 
 genius.searchSong(songQuery, "rap", songCallback);
